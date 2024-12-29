1c80d868efb1e8761a73e3a4aed9cf305fe10a69
LSH <lsh2613@naver.com>
2024-12-29 11:15:48 +0900

    feat: jwt 검증 로직 구현

--- src/main/java/com/pitchain/common/apiPayload/statusEnums/ErrorStatus.java
+++ src/main/java/com/pitchain/common/apiPayload/statusEnums/ErrorStatus.java
@@ -13,7 +13,10 @@ public enum ErrorStatus implements ResponseStatus {
     _INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON500", "서버 에러, 관리자에게 문의 바랍니다."),
     _BAD_REQUEST(HttpStatus.BAD_REQUEST, "COMMON400", "잘못된 요청입니다."),
     _UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "COMMON401", "인증이 필요합니다."),
-    _FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON403", "금지된 요청입니다.");
+    _FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON403", "금지된 요청입니다."),
+
+    // jwt
+    MISSING_ACCESS_TOKEN(HttpStatus.BAD_REQUEST, "TOKEN4012", "Access Token이 존재하지 않습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
